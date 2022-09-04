2106cbdc5dea94263422ba5edfcd35c65635421d
Sebastian Banz <sebastian.banz@yahoo.de>
2022-09-04 18:21:34 +0200

    First attempt

--- AoC2015/day24/script.py
+++ AoC2015/day24/script.py
@@ -1,5 +1,7 @@
 #!/usr/local/bin/python3
+from genericpath import exists
 import os
+# from math import prod
 
 # Part 1
 print("------- Part 1 -------")
@@ -10,25 +12,82 @@ with open(os.path.join(script_dir, "data.txt")) as file:
     for line in file:
         data.append(int(line))
 
-RELATIVE_WEIGHT = sum(data) / 3
+def get_relative_weight(data, compartments):
+    return sum(data) / compartments
 
-
-def generate_combinations(data, current):
+def generate_combinations(data):
     combinations = []
-    while True:
-        present = data.pop()
-        if sum(current) + present < RELATIVE_WEIGHT:
-            new = [x for x in current]
-            new.append(present)
-            generate_combinations(data, new)
-        if sum(current) + present == RELATIVE_WEIGHT:
-            combinations.append()
-            combinations[-1] = [x for x in current]
-            combinations[-1].append(present)
-        if sum(current) + present < RELATIVE_WEIGHT:
-
-
-possibilities = []
-while True:
     current = []
-    current_data = sorted([x for x in data])
+    data.sort(reverse=True)
+    i = 0
+    while i < len(data):
+        if sum(current) + data[i] < RELATIVE_WEIGHT:
+            current.append(data[i])
+            i += 1
+        elif sum(current) + data[i] == RELATIVE_WEIGHT:
+            current.append(data[i])
+            combinations.append(current.copy())
+            i = data.index(current[-1]) + 1
+            current.pop()
+        else:
+            i += 1
+        while i >= len(data):
+            if len(current) == 0:
+                break
+            i = data.index(current[-1]) + 1
+            current.pop()
+    return combinations
+
+
+# def get_valid_combinations(possibilities):
+#     valid = []
+#     for x in possibilities:
+#         setx = set(x)
+#         for y in possibilities:
+#             sety = set(y)
+#             if len(setx.intersection(sety)) == 0:
+#                 valid.append(x)
+#                 break
+#     return valid
+def get_valid_combinations(possibilities):
+    valid = []
+    min = 0
+    possibilities = sorted(possibilities)
+    for x in possibilities:
+        if len(x) > min and len(valid) == 0:
+            min = len(x)
+        if len(x) > min:
+            break
+        else:
+            setx = set(x)
+            for y in possibilities:
+                sety = set(y)
+
+
+def get_smallest_package_amount_combinations(valid_possibilities):
+    smallestamount = len(min(valid_possibilities, key=lambda pos: len(pos)))
+    return [pos for pos in valid_possibilities if len(pos) == smallestamount]
+
+
+def get_smallest_quantum_entanglement(optimal_possibilities):
+    optimum = -1
+    for pos in optimal_possibilities:
+        value = 1
+        for x in pos:
+            value *= x
+        optimum = value if optimum == -1 else min(value, optimum)
+    return optimum
+    # return min([prod(x) for x in optimal_possibilities])
+
+RELATIVE_WEIGHT = get_relative_weight(data, 3)
+original_data = sorted([x for x in data])
+print(f"Original data:\n{original_data}")
+possibilities = generate_combinations(original_data)
+print(f"Possible package combinations to achieve an average of {RELATIVE_WEIGHT}:\n{len(possibilities)}")
+valid = get_valid_combinations(possibilities)
+print(f"Valid combinations:\n{len(valid)}")
+optimalpossibilities = get_smallest_package_amount_combinations(valid)
+print(f"Optimal combinations, i.e. smallest amount of packages: \n{optimalpossibilities}")
+optimum = get_smallest_quantum_entanglement(optimalpossibilities)
+
+print(f"The optimal quantum entanglement in Santa's cabin is {optimum}.") # 10723906903
