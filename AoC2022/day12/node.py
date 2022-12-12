04d0d0a4ffeafd829b80550618fb96b2263b8f92
Sebanzian <sebastian.banz@yahoo.de>
2022-12-12 14:58:00 +0100

    day 12 wip

--- AoC2022/day12/node.py
+++ AoC2022/day12/node.py
@@ -8,48 +8,54 @@ for i, char in enumerate(ascii_lowercase, 1):
     VALUES[char] = i
 
 class Node:
-    
+    end = []
+    start = []
     nodes = dict()
     def __init__(self, x, y, char):
         self.x = x
         self.y = y
         self.value = char
+        if char == "E":
+            self.end.append(self)
+        elif char == "S":
+            self.start.append(self)
         self.shortest_path = 0
         self.nodes[f"({self.x}, {self.y})"] = self
-        self.targets = []
+        self.sources = []
 
     def __repr__(self):
         return f"{self.value} ({self.x}, {self.y})"
 
     @classmethod
     def find_shortest_path(cls):
-        start = None
-        for k, node in cls.nodes.items():
-            if k.startswith("S"):
-                start = node
-                break
-        start.get_shortest_path()
-        return start.shortest_path
+        path = []
+        path.append(cls.end[0])
+        cls.end[0].get_shortest_path(path)
+        return cls.start[0].shortest_path
 
 
-    def find_targets(self):
+    def find_sources(self):
         north = self.nodes.get(f"({self.x}, {self.y - 1})")
         south = self.nodes.get(f"({self.x}, {self.y + 1})")
         east = self.nodes.get(f"({self.x + 1}, {self.y})")
         west = self.nodes.get(f"({self.x - 1}, {self.y})")
-        if north != None and VALUES[north.value] <= VALUES[self.value] + 1:
-            self.targets.append(north)
-        if south != None and VALUES[south.value] <= VALUES[self.value] + 1:
-            self.targets.append(south)
-        if east != None and VALUES[east.value] <= VALUES[self.value] + 1:
-            self.targets.append(east)
-        if west != None and VALUES[west.value] <= VALUES[self.value] + 1:
-            self.targets.append(west)
+        if north != None and VALUES[north.value] + 1 >= VALUES[self.value]:
+            self.sources.append(north)
+        if south != None and VALUES[south.value] + 1 >= VALUES[self.value]:
+            self.sources.append(south)
+        if east != None and VALUES[east.value] + 1 >= VALUES[self.value]:
+            self.sources.append(east)
+        if west != None and VALUES[west.value] + 1 >= VALUES[self.value]:
+            self.sources.append(west)
+        self.sources.sort(key=lambda x: abs(self.start[0].x - x.x) + abs(self.start[0].y - x.y))
 
-    def get_shortest_path(self):
-        path = []
-        for target in self.targets:
-            if target.shortest_path == 0 and target.value != "E":
-                target.get_shortest_path()
-            path.append(target.shortest_path + 1)
-        self.shortest_path = min(path)
+
+    def get_shortest_path(self, path):
+        if self.value == "S":
+            return
+        for source in self.sources:
+            if source.value != "E" and (source.shortest_path == 0 or source.shortest_path > self.shortest_path + 1):
+                source.shortest_path = self.shortest_path + 1
+                path.append(source)
+                source.get_shortest_path(path)
+        path.pop()
--- AoC2022/day12/script.py
+++ AoC2022/day12/script.py
@@ -1,5 +1,8 @@
 import os
 from node import Node
+import sys
+
+sys.setrecursionlimit(10000)
 
 # Part 1
 print("------- Part 1 -------")
@@ -20,6 +23,13 @@ for y, row in enumerate(data):
         Node(x, y, char)
 
 for node in Node.nodes.values():
-    node.find_targets()
+    node.find_sources()
 
-print(Node.find_shortest_path())
+print(f"The shortest path takes {Node.find_shortest_path()} steps.")
+
+# res = ""
+# for node in sorted(Node.nodes.values(), key=lambda x: x.y):
+#     if node.x == 0:
+#         res += "\n"
+#     res += str(node.shortest_path) + " "
+# print(res)
