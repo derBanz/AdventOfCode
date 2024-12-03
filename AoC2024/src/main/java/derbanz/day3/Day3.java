ca57e9af6be7289cf136a106925a816467b139f3
Sebastian Banz <sebastian.banz@yahoo.de>
2024-12-03 20:53:28 +0100

    Day 3 part 2

--- AoC2024/src/main/java/derbanz/day3/Day3.java
+++ AoC2024/src/main/java/derbanz/day3/Day3.java
@@ -19,12 +19,22 @@ public class Day3 extends Day {
 
     @Override
     protected void doExecute(boolean isPartTwo, Instructions instructions, Instant start, boolean isTest) {
-        int result = getMultiplicationResult(instructions.getLines());
+        int result = getMultiplicationResult(instructions.getLines(), isPartTwo);
         Printer.printResult(String.valueOf(result), 3, isPartTwo ? 2 : 1, start, isTest);
     }
 
-    private int getMultiplicationResult(List<String> lines) {
+    private int getMultiplicationResult(List<String> lines, boolean isPartTwo) {
         String line = lines.stream().reduce("", String::concat);
+        if (isPartTwo) {
+            List<String> dontSplit = new ArrayList<>(List.of(line.split("don't\\(\\)")));
+            List<String> doSplit = new ArrayList<>(List.of(dontSplit.removeFirst()));
+            dontSplit.forEach(dont -> {
+                List<String> dos = new ArrayList<>(List.of(dont.split("do\\(\\)")));
+                dos.removeFirst();
+                doSplit.addAll(dos);
+            });
+            line = doSplit.stream().reduce("", String::concat);
+        }
         return Pattern.compile("mul\\(\\d+,\\d+\\)").matcher(line).results()
                 .map(result -> Pattern.compile("\\d+").matcher(result.group()).results()
                         .map(r -> Integer.valueOf(r.group()))
